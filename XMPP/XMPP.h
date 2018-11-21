7c9147fffd3cf9aabd6ad360206346b5c5377e9b
Mickael Remond <mremond@process-one.net>
2018-11-21 11:26:10 +0100

    Initial Commit

---
+++ XMPP/XMPP.h
@@ -0,0 +1,19 @@
+//
+//  XMPP.h
+//  XMPP
+//
+//  Created by Mickaël Rémond on 21/11/2018.
+//  Copyright © 2018 ProcessOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMPP.
+FOUNDATION_EXPORT double XMPPVersionNumber;
+
+//! Project version string for XMPP.
+FOUNDATION_EXPORT const unsigned char XMPPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPP/PublicHeader.h>
+
+
---
+++ XMPPTests/XMPPTests.swift
@@ -0,0 +1,34 @@
+//
+//  XMPPTests.swift
+//  XMPPTests
+//
+//  Created by Mickaël Rémond on 21/11/2018.
+//  Copyright © 2018 ProcessOne. All rights reserved.
+//
+
+import XCTest
+@testable import XMPP
+
+class XMPPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
