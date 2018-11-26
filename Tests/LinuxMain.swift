cddc0fc9f00986e64ec344b66ef52d772b68cef6
Mickael Remond <mremond@process-one.net>
2018-11-26 12:05:06 +0100

    Add missing file for Linux build

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,15 @@
+//
+//  LinuxMain.swift
+//  XMPP
+//
+//  Created by Mickaël Rémond on 26/11/2018.
+//  Copyright © 2018 ProcessOne. All rights reserved.
+//
+
+import XCTest
+
+import XMPPTestsTests
+
+var tests = [XCTestCaseEntry]()
+tests += XMPPTestsTests.allTests()
+XCTMain(tests)
